fdceab3c233d8f000d34a3eaff5da95e348d7166
Fran1611 <francisco.ibanez@correo.ucu.edu.uy>
2020-04-11 14:10:47 -0300

    subo program con mi nombre

--- Program/Program.cs
+++ Program/Program.cs
@@ -8,6 +8,7 @@ namespace Program
         {
             Console.WriteLine("Mauricio Gómez");
             Console.WriteLine("Nahuel Costa");
+            Console.WriteLine("Francisco Ibañez");
         }
     }
 }
